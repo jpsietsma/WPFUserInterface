d24e8303842b568ef266b734aed59a4d4bc7f63a
Jimmy S <jpsietsma@gmail.com>
2019-03-05 14:56:46 -0500

    Added DI

---
+++ WPFUserInterface/ViewModels/ShellViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFUserInterface.ViewModels
+{
+    public class ShellViewModel
+    {
+        private ICalculations _calculations;
+
+        public ShellViewModel(ICalculations calculations)
+        {
+            _calculations = calculations;
+        }
+    }
+}
